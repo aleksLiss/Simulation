95421d1024d21525bdadf7828e89897827e8b20c
aleksLiss <darklisik.darklisik@yandex.ru>
2024-05-05 15:29:39 +0300

    add Map.java

--- src/main/java/Simulation/Map/Map.java
+++ src/main/java/Simulation/Map/Map.java
@@ -1,4 +1,20 @@
 package Simulation.Map;
 
+import Simulation.Entityis.BasicEntity.Entity;
+import Simulation.Entityis.MoovableEntytyisImpl.Herbivore;
+import Simulation.Entityis.MoovableEntytyisImpl.Predator;
+
+import java.util.HashMap;
+
 public class Map {
+    private Map<Cell, Herbivore> herbivores;
+    private Map<Cell, Predator> predators;
+    public Map() {
+        this.herbivores = new HashMap<>();
+        this.predators = new HashMap<>();
+    }
+
+
+
+
 }
