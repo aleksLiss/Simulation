3ae9ba359c619d3530afaf6fc1422040b9c7e6da
aleksLiss <darklisik.darklisik@yandex.ru>
2024-05-07 10:59:01 +0300

    change GrassesAndHerbivoreAndPredatorsStorage

--- src/main/java/Simulation/GrassesAndHerbivoresAndPredatorsOnMap.java
+++ src/main/java/Simulation/GrassesAndHerbivoresAndPredatorsOnMap.java
@@ -12,84 +12,13 @@ import java.util.List;
 import java.util.Map;
 
 public class GrassesAndHerbivoresAndPredatorsOnMap {
-    private Map<Coordinate, Herbivore> herbivores;
-    private Map<Coordinate, Grass> grasses;
-    private Map<Coordinate, Predator> predators;
     private Map<Coordinate, Entity> staticStorage;
     private Map<Coordinate, Creature> moovableStorage;
     public GrassesAndHerbivoresAndPredatorsOnMap() {
-        this.herbivores = new HashMap<>();
-        this.predators = new HashMap<>();
-        this.grasses = new HashMap<>();
         this.staticStorage = new HashMap<>();
         this.moovableStorage = new HashMap<>();
     }
 
-    /*
-    public void putEntityToStorage(Switcher switcher, Coordinate coordinate, Entity entity) {
-        switch (switcher) {
-            case GRASS:
-                grasses.put(coordinate, (Grass) entity);
-                break;
-            case HERBIVORE:
-                herbivores.put(coordinate, (Herbivore) entity);
-                break;
-            case PREDATOR:
-                predators.put(coordinate, (Predator) entity);
-                break;
-        }
-    }
-    public void removeEntityFromMap(Switcher switcher, Entity entity) {
-        switch (switcher) {
-            case GRASS:
-                grasses.remove((Grass) entity);
-                break;
-            case HERBIVORE:
-                herbivores.remove((Herbivore) entity);
-                break;
-            case PREDATOR:
-                predators.remove((Predator) entity);
-                break;
-        }
-    }
-
-    public Coordinate getCoordinateOnMap(Switcher switcher, Entity entity) {
-        Coordinate coordinate = null;
-        switch (switcher) {
-            case GRASS:
-                for (Map.Entry entry : grasses.entrySet()) {
-                    coordinate = (Coordinate) entry.getKey();
-                }
-                break;
-            case HERBIVORE:
-                for (Map.Entry entry : herbivores.entrySet()) {
-                    if(entity.equals(entry.getValue())){
-                        coordinate = (Coordinate) entry.getKey();
-                    }
-                }
-                break;
-            case PREDATOR:
-                for (Map.Entry entry : predators.entrySet()) {
-                    coordinate = (Coordinate) entry.getKey();
-                }
-                break;
-        }
-        return coordinate;
-    }
-
-    public Map<Coordinate, Herbivore> getHerbivores(){
-        return herbivores;
-    }
-
-    public Map<Coordinate, Grass> getGrasses() {
-        return grasses;
-    }
-
-    public Map<Coordinate, Predator> getPredators() {
-        return predators;
-    }
-
-    */
     public Map<Coordinate, Entity> getStaticStorage() {
         return staticStorage;
     }
@@ -104,6 +33,13 @@ public class GrassesAndHerbivoresAndPredatorsOnMap {
 
     public void putIntoMoovableStorage(Coordinate coordinate, Creature creature){
         moovableStorage.put(coordinate, creature);
+    }
+
+    public void removeFromStaticStorage(Entity entity){
+        staticStorage.remove(entity);
+    }
+    public void removeFromMoovableStorage(Creature creature){
+        moovableStorage.remove(creature);
     }
 }
 
